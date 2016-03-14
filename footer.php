1a975a8e4623c6eeea78619cb688b1d53878d8a0
Hamed Nourhani <info@itstar.ir>
2016-03-14 17:37:45 +0330

    first commit

--- footer.php
+++ footer.php
@@ -13,9 +13,12 @@
 				<div class="credit-holder">
 					<section class="layout">
 						
-						<span class="site-holder">
-							<?php echo __('Designed by','patiab').'<a href="http://Karait.com">'.' '.__('Farakaranet','patiab').'</a>'; ?>
-						</span>
+						<div class="site-holder">
+							طراحی شده توسط
+							<a class="farakaranet" href='http://karait.com'>
+								<?php echo __('Farakaranet','patiab');?>
+							</a>
+						</div>
 						
 						<span class="credit">
 							<?php echo __('patiab © 2015. All Right Reserved','patiab');?>
